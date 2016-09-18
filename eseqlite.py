332614cc02fb25639279826760bbd8f22e259a80
crisconru <crisconru@gmail.com>
2016-09-18 10:40:07 +0200

    Un bug en la busqueda de codigo en la bbdd arreglado

--- eseqlite.py
+++ eseqlite.py
@@ -1,37 +1,18 @@
 import sqlite3
 
+''' Make a string query from a list'''
 def make_query(components):
-	query = ' ('
-	i = 0
+	query = '('
 	for component in components:
-		query = query + component
-		if (i < len(components) -1):
-			query = query + ', '
-		else :
-			query = query + ')'
-		i = i + 1
-	#print query
-	return query
+		query += component + ','
+	query[len(query)-1] = ')'
+	return query# str(query)
 
-def make_fields(fields):
-	query = ''
-	i = 0
-	for field in fields:
-		query = query + field
-		if (i < len(fields) -1):
-			query = query + ', '
-		else :
-			query = query + ' '
-		i = i + 1
-	#print query
-	return query
-
-def make_values(num):
+def make_values(vals):
 	values = '('
-	for i in range(num-1):
-		values = values + '?, '
-	values = values + '?)'
-	#print values
+	for i in range(len(vals)-1):
+		values += '?, '
+	values += '?)'
 	return values
 
 def make_inputs(fields):
@@ -49,7 +30,7 @@ def make_inputs(fields):
 
 def createTable(ddbb, table, fields):
 	query = 'Create table if not exists ' + str(table) + make_query(fields)
-	print query
+	print 'createTable()\n', query
 	# ddbb
 	conn = sqlite3.connect(ddbb)
 	c = conn.cursor()
@@ -57,7 +38,19 @@ def createTable(ddbb, table, fields):
 	conn.commit()
 	conn.close()
 
-def insertTable(ddbb, table, fields, values):
+def insert(ddbb, table, values):
+	query = 'INSERT INTO ' + str(table) + ' VALUES '
+	query += make_values(values)
+	valuesTuple = tuple(values)
+	vals = [valuesTuple]
+	# ddbb
+	conn = sqlite3.connect(ddbb)
+	c = conn.cursor()
+	c.executemany(query, vals)
+	conn.commit()
+	conn.close()
+'''
+def insert(ddbb, table, fields, values):
 	query = 'INSERT OR IGNORE INTO ' + table + make_query(fields) + ' VALUES '
 	query = query + make_values(len(fields))
 	print query
@@ -80,4 +73,4 @@ def selectFields(ddbb, table, fields, values):
 	conn.commit()
 	conn.close()
 	#return result
-	
+'''
--- example.py
+++ example.py
@@ -1,30 +1,38 @@
 import eseqlite
-# dbb
+import datetime
+
+''' DDBB '''
 ddbb = 'example.db'
-# table
-table = 'table_example'
-# fields
+
+''' Table 1 '''
+table1 = 'table_1'
+# Columns - Fields of the Table 1
+pos  = 'pos integer primary key autoincrement'
+code = 'codigo str unique'
+type = 'tipo str'
+used = 'usado int'
+when = 'cuando numeric'
 fields = []
-id = 'id integer primary key autoincrement'
-fields.append(id)
-code = 'code str unique'
+fields.append(pos)
 fields.append(code)
-type = 'type str'
 fields.append(type)
-print 'Fields = ' + str(fields)
-# create table
-eseqlite.createTable(ddbb, table, fields)
-# update fields
-values = [('2006-03-28', 'BUY'),
-             ('2006-04-05', 'MSFT'),
-             ('2006-04-06', 'SELL'),
-            ]
+fields.append(used)
+fields.append(when)
+
+''' Create Table 1'''
+eseqlite.createTable(ddbb, table1, fields)
+
+''' Insert data into Table 1 '''
+values = [1234, 'peq', 0, datetime.datetime.now()]
+#eseqlite.insert(ddbb, table1, values)
+
+'''
 fields2 = []
 fields2.append('code')
-fields2.append('type')
+fields2.append('type')'''
 '''print 'Fields2 = ' + str(fields2)
 print 'Values = ' + str(values)
 eseqlite.insertTable(ddbb, table, fields2, values)
 print eseqlite.make_fields(fields2)
 print eseqlite.make_inputs(fields2)'''
-print eseqlite.selectFields(ddbb,table,'code', '2006-04-05')
+#print eseqlite.selectFields(ddbb,table,'code', '2006-04-05')
